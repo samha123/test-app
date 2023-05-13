c4ce1f6d11590e153be3fdf3b82f3c2cb55a517a
samha123 <zsamha0@gmail.com>
2023-05-13 12:31:09 +0400

    changed

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,8 @@ use App\Http\Controllers\ProductController;
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
     //All secure URL's
-    Route::get("/products",[ProductController::class,'index']);
+  
 
     });
 Route::post("/public/login",[UserController::class,'index']);
+Route::get("/products",[ProductController::class,'index']);
